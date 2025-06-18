751bc02c72e5df84958b428da29179ee6478d8ad
Erik van Velzen <erik@evanv.nl>
2025-06-18 16:32:08 +0200

    remove boilerplate

--- src/main/java/energy/lux/Main.java
+++ src/main/java/energy/lux/Main.java
@@ -3,7 +3,6 @@ package energy.lux;
 import esdl.Area;
 import esdl.EnergySystem;
 import esdl.EsdlFactory;
-import esdl.impl.EnergySystemImpl;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
@@ -15,37 +14,27 @@ import java.util.HashMap;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        var esdlFactory = EsdlFactory.eINSTANCE;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-
-        var factory = EsdlFactory.eINSTANCE;
-
-        EnergySystem energySystem = EsdlFactory.eINSTANCE.createEnergySystem();
+        EnergySystem energySystem = esdlFactory.createEnergySystem();
         energySystem.setName("My First EnergySystem");
         energySystem.setId("firstEnergySystem");
 
         var instances = energySystem.getInstance();
-        var instance = factory.createInstance();
+        var instance = esdlFactory.createInstance();
         instances.add(instance);
 
-        Area area = EsdlFactory.eINSTANCE.createArea();
+        Area area = esdlFactory.createArea();
         instance.setArea(area);
         area.setId("Test");
         area.setName("Amsterdam municipality");
 
-        var pvInstallation = factory.createPVInstallation();
+        var pvInstallation = esdlFactory.createPVInstallation();
         pvInstallation.setNumberOfPanels(42);
         area.getAsset().add(pvInstallation);
 
-        var measure = EsdlFactory.eINSTANCE.createMeasure();
-        var measures = EsdlFactory.eINSTANCE.createMeasures();
+        var measure = esdlFactory.createMeasure();
+        var measures = esdlFactory.createMeasures();
 
         energySystem.setMeasures(measures);
 
